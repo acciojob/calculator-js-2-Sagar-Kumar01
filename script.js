a07f34aa4c9d42ea8b7ec459a722c1dff6c84d41
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-10-21 21:32:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let C = document.getElementById("C");
+let back = document.getElementById("back");
+let inputs = document.getElementById("inputs");
+
+C.addEventListener("click",()=>{
+	inputs.value = "";
+});
+
+back.addEventListener("click",()=>{
+	if(inputs.value != ""){
+		deleteone(inputs.value)
+	}
+	
+});
+
+function deleteone(str){
+	let arr = str.split("");
+	arr.pop();
+	inputs.value = arr.join("")
+}
+
